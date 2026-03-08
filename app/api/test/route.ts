2ab5ea5ab9f13143c9820ec148fb5aa7cc1b099f
agarwalsakshi20 <sakshiagarwal8186@gmail.com>
2026-03-08 20:34:39 +0530

    database connection done

---
+++ app/api/test/route.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/lib/prisma"
+
+export async function GET() {
+
+  const users = await prisma.user.findMany()
+
+  return Response.json({
+    message: "Database working",
+    users
+  })
+
+}
